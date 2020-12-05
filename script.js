6c89a4b71376b215a37816a956cd9f68924305c0
Dg-byte <63142225+Dg-byte@users.noreply.github.com>
2020-12-05 20:02:07 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,9 @@
-let ctx = field.getContext("2d");
-let player = {
-    sprite: new Image(100, 100)  ,
-    x : 0 ,
-    y : 0 ,
-}
-
-
-field.width = window.innerWidth;
-field.height = window.innerHeight;
-
-
-function draw() {
-    let bg = new Image (field.width, field.height);
-    bg.src = "img/background.JPG";
-    bg.onload = () => {
-        ctx.drawImage(bg,0,0);
+window.onscroll = function(e){
+    console.log(window.scrollY)
+    if(window.scrollY >= 969 && window.scrollY < 1200){
+        document.querySelector(".abm").id ="fixed";
     }
-    
-    player.sprite = new Image(100,100);
-    player.sprite.src = "img/player.png";
-    player.sprite.onload = () =>
-        ctx.drawImage(player.sprite, player.x, player.y);
+    else if(window.scrollY >= 1200){
+        document.querySelector(".abm").id="";
+    }
 }
- 
-
-
-$(field).on("mousemove",function(e){
-    player.x = e.offsetX;
-    player.y = e.offsetY;
-    draw();
-
-});
-draw();
